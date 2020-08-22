2382e0ff0169a331adb59be59a02ae36bf417f5b
Halyna Urbanska <webenot@mail.ua>
2020-08-22 11:02:29 +0300

    Create Cat

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { ICat } from './interfaces/cat.interface';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
@@ -10,5 +11,10 @@ export class CatsController {
   @Get()
   async findAll(): Promise<ICat[]> {
     return this.catsService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto): Promise<ICat> {
+    return this.catsService.create(createCatDto);
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { ICat } from './interfaces/cat.interface';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Injectable()
 export class CatsService {
@@ -9,5 +10,9 @@ export class CatsService {
   }
   async findAll(): Promise<ICat[]> {
     return await this.catModel.find().exec();
+  }
+  async create(createCatDto: CreateCatDto): Promise<ICat> {
+    const createdCat = new this.catModel(createCatDto);
+    return createdCat.save();
   }
 }
